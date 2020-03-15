b014af5a996a9931c65fc17c9b6be5c8bf1f0e8c
Minakshi Chidrawar <minakshichidrawar@gmail.com>
2020-03-15 20:49:36 +0000

    Added description for the game

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -1,6 +1,21 @@
 @extends('layouts.app')
 
 @section('content')
+    <ul class="nobullet">
+        <li>This is a two player card game</li>
+        <li>The game starts with a deck of cards</li>
+        <li>The cards are dealt out to both players</li>
+    </ul>
+
+    <div class="justify-text-center" style="margin-left: 28%">
+        <ol class="text-left">
+            <h5>On each turn:</h5>
+            <li>Both players turn over their top-most card</li>
+            <li>The player with the higher valued card takes the cards and <br>puts them in their scoring pile (scoring 1 point per card)</li>
+            <li>This continues until the players have no cards left</li>
+            <li>The player with the highest score wins</li>
+        </ol>
+    </div>
     <div class="alignStartWelcomeButton">
         <a href="{{ route('startgame') }}" class="btn btn-warning">Start Game</a>
     </div>
