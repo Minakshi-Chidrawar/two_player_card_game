1cc538f9c1bb44abb4a3304caabfe22ff0b8cb7b
Minakshi Chidrawar <minakshichidrawar@gmail.com>
2020-03-19 21:24:49 +0000

    Amended to display buttons one by one

--- resources/views/games/footerButton.blade.php
+++ resources/views/games/footerButton.blade.php
@@ -1,7 +1,11 @@
-<div class="row my-4 d-flex justify-content-center">
-    <a href="{{ route('nextturn') }}" class="btn btn-warning">Next turn</a>
+<div class="row mt-5">
+    <div class="offset-53">
+        <a href="{{ route('nextturn') }}" class="btn btn-warning">Next turn</a>
+    </div>
+</div>
 
-    <div class="offset-md-1">
+<div class="row my-4">
+    <div class="offset-525">
         <a href="{{ route('startgame') }}" class="btn btn-danger">New Game</a>
     </div>
 </div>
