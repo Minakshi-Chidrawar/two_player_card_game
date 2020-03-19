04aabd84984035b90788a1d59e9a770a2be57fcf
Minakshi Chidrawar <minakshichidrawar@gmail.com>
2020-03-19 21:26:01 +0000

    Added back deck image

--- resources/views/games/startGame.blade.php
+++ resources/views/games/startGame.blade.php
@@ -2,20 +2,22 @@
 
 @section('content')
     <div class="row">
+        <div class="col-sm-2">
+
+        </div>
         <!-- Player 1 -->
-        <div class="col-sm-6">
+        <div class="col-sm-4">
             <h1 class="headerFont">Player 1</h1>
             <h4>Score: {{ session()->get('player1_score')}}</h4>
-            <div class="card">
-            </div>
+            
+            <img src="{{ asset('images/backDeck.png') }}" class="backDeckImg mt-2" alt="Deck card for player 1">
         </div>
 
         <!-- Player 2 -->
-        <div class="col-sm-6">
+        <div class="col-sm-3">
             <h1 class="headerFont">Player 2</h1>
             <h4>Score: {{ session()->get('player2_score')}}</h4>
-            <div class="card">
-            </div>
+            <img src="{{ asset('images/backDeck.png') }}" class="backDeckImg mt-2" alt="Deck card for player 2">
         </div>
     </div>
     
