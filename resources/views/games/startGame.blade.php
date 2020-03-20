b2f4485c02677ffd18bb608c728e38409324690e
Minakshi Chidrawar <minakshichidrawar@gmail.com>
2020-03-20 17:37:27 +0000

    Minor change

--- resources/views/games/startGame.blade.php
+++ resources/views/games/startGame.blade.php
@@ -2,11 +2,8 @@
 
 @section('content')
     <div class="row">
-        <div class="col-sm-2">
-
-        </div>
         <!-- Player 1 -->
-        <div class="col-sm-4">
+        <div class="col-sm-6">
             <h1 class="headerFont">Player 1</h1>
             <h4>Score: {{ session()->get('player1_score')}}</h4>
             
@@ -14,7 +11,7 @@
         </div>
 
         <!-- Player 2 -->
-        <div class="col-sm-3">
+        <div class="col-sm-5">
             <h1 class="headerFont">Player 2</h1>
             <h4>Score: {{ session()->get('player2_score')}}</h4>
             <img src="{{ asset('images/backDeck.png') }}" class="backDeckImg mt-2" alt="Deck card for player 2">
